40feb7e13fbcfd7c3c724d0ede248156a5467003
noahcheng119 <82508882+noahcheng119@users.noreply.github.com>
2022-04-14 15:54:37 +0800

    Initial commit

---
+++ src/simple/SumOfTwoNumber.java
@@ -0,0 +1,19 @@
+package simple;
+
+import java.util.Arrays;
+
+public class SumOfTwoNumber {
+    public static void main(String[] args) {
+        int[] s = {3,2,1,2,5};
+        int i = 7;
+        System.out.println(Arrays.toString(twoSum(s,i)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+
